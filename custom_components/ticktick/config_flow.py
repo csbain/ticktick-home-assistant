09217003f9de8790694f3e00b12edfdf6429c2c6
Chris Bain <christophersbain@gmail.com>
2026-02-20 15:50:22 +1000

    feat: add completed days input to config flow

--- custom_components/ticktick/config_flow.py
+++ custom_components/ticktick/config_flow.py
@@ -2,9 +2,16 @@
 
 import logging
 
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.components.http import async_import_module
 from homeassistant.helpers import config_entry_oauth2_flow
 
-from .const import DOMAIN
+from .const import (
+    CONF_COMPLETED_TASKS_DAYS,
+    DEFAULT_COMPLETED_TASKS_DAYS,
+    DOMAIN,
+)
 
 
 class OAuth2FlowHandler(
@@ -18,3 +25,34 @@ class OAuth2FlowHandler(
     def logger(self) -> logging.Logger:
         """Return logger."""
         return logging.getLogger(__name__)
+
+    @staticmethod
+    def async_get_options_flow(
+        config_entry: config_entries.ConfigEntry,
+    ) -> config_entries.OptionsFlow:
+        """Get the options flow for this handler."""
+        return TickTickOptionsFlowHandler(config_entry)
+
+
+class TickTickOptionsFlowHandler(config_entries.OptionsFlow):
+    """Config flow options handler for TickTick."""
+
+    async def async_step_init(self, user_input=None):
+        """Manage options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        options = self.config_entry.options
+        completed_days = options.get(
+            CONF_COMPLETED_TASKS_DAYS, DEFAULT_COMPLETED_TASKS_DAYS
+        )
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_COMPLETED_TASKS_DAYS, default=completed_days
+                ): vol.All(vol.Coerce(int), vol.Range(min=1, max=365))
+            }
+        )
+
+        return self.async_show_form(step_id="init", data_schema=data_schema)
